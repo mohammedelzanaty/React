f7583d2a80fff78a3d36f784d98b43e987c48d42
Mohammed <mohammedelzanaty129@gmail.com>
2019-02-17 16:33:00 +0200

    [ F, Intro2ReactV4 ] Configuration

---
+++ IntroToReactV4/src/js/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Pet from './Pet';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Adopt Me!</h1>
+        <Pet name="Luna" animal="Dog" breed="Havanese" />
+        <Pet name="Luna" animal="Dog" breed="Havanese" />
+        <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+        <Pet name="Doink" animal="Cat" breed="Mix" />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# React
+# My React Learning Path <img src="http://bit.ly/2SXZHZY" width="125" align="right" alt="JS Logo">
+
+> Take your React to the next level and find out what it's fully capable of.
+
+React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.
