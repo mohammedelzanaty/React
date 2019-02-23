a70e53a83b0dc242eab49243e186a127f03da3b8
Mohammed <mohammedelzanaty129@gmail.com>
2019-02-23 19:05:08 +0200

    init chat app

--- reactjs-articles/src/js/Home.js
+++ reactjs-articles/src/js/Home.js
@@ -3,17 +3,17 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-        <ul>
-          <li>
-            <a
-              href="https://hackernoon.com/everything-you-need-to-know-about-reacts-context-api-e5c8c32ef202"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Everything you need to know about React’s Context API
-            </a>
-          </li>
-        </ul>
+      <ul>
+        <li>
+          <a
+            href="https://hackernoon.com/everything-you-need-to-know-about-reacts-context-api-e5c8c32ef202"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Everything you need to know about React’s Context API
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
